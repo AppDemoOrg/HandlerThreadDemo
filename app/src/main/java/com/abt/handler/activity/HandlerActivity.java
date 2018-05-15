009036773629a87e5278bd1931de75eecbe839ba
huangweiqi <askviky2010@gmail.com>
2018-05-15 21:33:08 +0800

    完善Handler

--- app/src/main/java/com/abt/handler/activity/HandlerActivity.java
+++ app/src/main/java/com/abt/handler/activity/HandlerActivity.java
@@ -12,52 +12,48 @@ import com.abt.handler.R;
 /**
  * Created by huangweiqi on 27/04/2018.
  */
-public class HandlerActivity extends AppCompatActivity {
+public class HandlerActivity extends AppCompatActivity implements Handler.Callback {
 
-    private Handler mMainHandler;// = new Handler();
+    private Handler mMainHandler;
     private TextView tvMain;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvMain = findViewById(R.id.tv_main);
+        SleepRunnable runnable = new SleepRunnable();
+        new Thread(runnable).start();
 
-        mMainHandler = new Handler(new Handler.Callback() {
-            @Override
-            public boolean handleMessage(Message msg) {
-                String result = "每隔2秒更新一下数据：";
-                result += Math.random();
-                tvMain.setText(result);
-                // 这里已经是主线程了
-                tvMain.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        // ...
-                    }
-                });
-                return true;
-            }
-        });
+        tvMain = findViewById(R.id.tv_main);
+        mMainHandler = new Handler(this);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    public boolean handleMessage(Message msg) {
+        /*try {
+            String threadName = Thread.currentThread().getName();
+            Logger.d(threadName+" --> doing long-running operations.");
+            Thread.sleep(1*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        String result = "每隔2秒更新一下数据：";
+        result += Math.random();
+        tvMain.setText(result);
+        return true;
+    }
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    // 这里是子线程
-                    Message.obtain(mMainHandler).sendToTarget();
+    class SleepRunnable implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(2*1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
+                Message.obtain(mMainHandler).sendToTarget();
             }
-        }).start();
+        }
     }
 }
